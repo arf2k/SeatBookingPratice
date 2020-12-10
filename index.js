54ac223634c88e0dd62e9aa46428797a5b320c36
https://learn.co/arf2k <foreman.alexander@gmail.com>
2020-12-10 16:17:59 -0500

    js minus one mistake

--- index.js
+++ index.js
@@ -2,10 +2,23 @@ const container = document.querySelector(".container");
 const seats = document.querySelectorAll(".row .seat:not(.occupied)");
 // grab everything with seat selector
 const count = document.querySelector("#count");
-const total = document.querySelector("#total");
+const total = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
-const ticketPrice = parseInt(movieSelect.value);
+// const ticketPrice = movieSelect.value;
+
+const updateSelectedCount = () => {
+  const selectedSeats = document.querySelectorAll(".row .seat.selected");
+  const selectedSeatsCount = selectedSeats.length;
+  count.innerText = selectedSeatsCount;
+  total.innertext = selectedSeatsCount * movieSelect.value;
+};
 
 container.addEventListener("click", (e) => {
-  console.log(e.target);
+  if (
+    e.target.classList.contains("seat") &&
+    !e.target.classList.contains("occupied")
+  ) {
+    e.target.classList.toggle("selected");
+    updateSelectedCount();
+  }
 });
